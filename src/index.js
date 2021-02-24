a6a8eb0e5e0b39d7530346f8e020e174902a1b6b
killcodings <kontaktid@ya.ru>
2021-02-24 16:28:18 +0300

    added functionality towel-sort to the file index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+
+
+  if (typeof matrix === "undefined") {
+      return [];
+  }
+  else {
+      let mass = [];
+      for (i = 0; i < matrix.length; i++) {
+          if (i % 2 == 0) {
+
+              for (j = 0; j < matrix[i].length; j++) {
+
+                  mass.push(matrix[i][j])
+              }
+          } else {
+              let matx = matrix[i].reverse()
+              for (k = 0; k < matx.length; k++) {
+
+                  mass.push(matx[k])
+
+              }
+          }
+
+      }
+      console.log(mass)
+      return mass;
+  }
 }
